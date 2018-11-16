e7bf746ec1283ae02430d8fb6232de65cc58fd48
Kramerbaf <nds.kram@gmail.com>
2018-11-16 16:18:37 +0100

    updated wwObject.register

--- src/index.js
+++ src/index.js
@@ -5,18 +5,22 @@ const wwEnableMetrics = false;
 
 const addComponent = function () {
     if (window.vm) {
-        window.vm.addComponent(name, wwObject);
 
-        wwLib.wwObject.registerContentType(
-            name,
-            {
+        wwLib.wwObject.register({
+            content: {
                 type: name,
                 data: {
                     size: "medium" // xbig, big, medium, small, xsmall
                 }
             },
-            wwEnableMetrics
-        );
+            enableMetrics: wwEnableMetrics,
+            /* wwManager:start */
+            cmsOptions: {
+            }
+            /* wwManager:end */
+        });
+
+        window.vm.addComponent(name, wwObject);
 
         return true;
     }
